ef89d83be0ad640c0e8ca3a70f0589973e34f92b
jkh <ajdkfl6445@gmail.com>
2019-02-20 11:43:36 +0900

    클래스패스 변

--- src/Ex04_Inherit_override.java
+++ src/Ex04_Inherit_override.java
@@ -2,12 +2,46 @@
  Today Point : [상속 관계]에서 override : 상속관계에서의 매서드 재정의
  - 상속관계에서 자식 클래스가 부모 클래스의 메서드의 재정의
  - 재정의(함수의 형태는 변화가 없고... 내용만 바뀌는 것)
+ - 틀의 변화가 아니라 내용의 변화! {함수의 실행 블럭 내용 변경} 
+ 
+ 문법) 상속관계에서 ...
+ 1. 부모함수의 이름과 동일
+ 2. 부모함수의 parameter 동일
+ 3. 부모함수의 return type 동일
+ 4. 결국에는 {} 괄호안에 내용 변경 
  */
+
+class Point3{
+	int x =4;
+	int y = 5;
+	
+	String getPosition() {
+		return "x :" + this.x + "y :"+ this.y;
+	}
+}
+
+// 3개의 점이 필요하다.
+// 2개의 점을 가지고 있는 설계도...
+class Point3D extends Point3{
+	int z =6;
+	
+	//String getPosition3d() {return ...}
+	//또 하나의 이름을 가진 함수를 만들고 싶지 않아요
+	
+	
+	
+}
+
+
+
 public class Ex04_Inherit_override {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Point3D point = new Point3D();
+		point.x = 3;
+		point.y = 6;
+		point.z = 9;
+		String result = point.getPosition();
+		System.out.println(result);
 	}
 
 }
